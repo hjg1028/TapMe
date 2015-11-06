923e1d30035fc9e2e386ed3386eeabc5c66141c8
hjg1028 <hjg1028@163.com>
2015-11-06 23:19:14 +0800

    add record tap time function

--- Tap Me/ViewController.swift
+++ Tap Me/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
@@ -16,14 +17,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var timer10Btn: UIButton!
     @IBOutlet weak var timer30Btn: UIButton!
     @IBOutlet weak var timer60Btn: UIButton!
+    @IBOutlet weak var tapButton: UIButton!
     
     var score: Int = 0
     var seconds: Int = 0
     var timer = NSTimer()
     var timeSetting: Int = 0
+    var tapTime: [String] = []
     
+    //计时初始化
     func startGame(setTime: Int) {
         score = 0
+        tapTime = []
         seconds = setTime
         timeLabel.text = "\(seconds)"
         scoreLabel.text = "\(score)"
@@ -31,6 +36,7 @@ class ViewController: UIViewController {
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("subtractTime"), userInfo: nil, repeats: true)
     }
     
+    //计时器及及时完成后弹出Alert
     func subtractTime() {
         
         seconds--
@@ -48,6 +54,11 @@ class ViewController: UIViewController {
             let alert = UIAlertController(title: "Time is up!", message: "You scored \(score) points in \(timeSetting) seconds. \(tapFrecuency) ms per tap on average.", preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "Play Again", style: UIAlertActionStyle.Default, handler: {ACTION in self.startGame(self.timeSetting)}))
             presentViewController(alert, animated: true, completion: nil)
+            
+            //输出纪录的点击时间到控制台
+            for n in 0...tapTime.count - 1 {
+                print(tapTime[n])
+            }
         }
         
     }
@@ -55,17 +66,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //绘制圆形按钮
+        tapButton.layer.cornerRadius = 100
+        tapButton.layer.masksToBounds = true
+        
+        //保持屏幕常亮
+        UIApplication.sharedApplication().idleTimerDisabled = true
     }
     
+    
+    //点击Tap按钮时
     @IBAction func buttonTapped(sender: AnyObject) {
         
         if seconds > 0 {
             score++
+            recordTapTime()
             scoreLabel.text = "\(score)"
         }
         
     }
     
+    //选择时长并开始计时
     @IBAction func timer10BtnTapped(sender: AnyObject) {
         resetButtonsAndTimer()
         timer10Btn.selected = true
@@ -87,11 +108,19 @@ class ViewController: UIViewController {
         startGame(timeSetting)
     }
     
+    //清除时长按钮选择状态及timer归零
     func resetButtonsAndTimer() {
         timer10Btn.selected = false
         timer30Btn.selected = false
         timer60Btn.selected = false
         timer.invalidate()
+    }
+    
+    //记录每次点击的时间
+    func recordTapTime() {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        tapTime.append(formatter.stringFromDate(NSDate()))
     }
     
     override func didReceiveMemoryWarning() {
